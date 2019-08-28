b44ad77de2216c50b9ef63cc1e6676793eb4b950
Nicholas Santantonio <ns722@cornell.edu>
2019-08-28 06:22:53 -0400

    replaced reshape2::melt, with meltName

--- R/dataFormat.R
+++ R/dataFormat.R
@@ -6,8 +6,13 @@
 #' @return formatted data frame for use inside bilinear()
 #' @keywords bilinear
 #' @export
-	dataFormat <- function(...){
-
+dataFormat <- function(...){
+# x = x; G = G; E = E; y = y; block = block; alpha = alpha; anyNullGEy = anyNullGEy
+	meltName <- function(x, vName) {
+		melted <- data.frame(G = rep(rownames(x), ncol(x)), E = rep(colnames(x), each = nrow(x)))
+		melted[vName] <- c(x)
+		melted
+	}
 		warnmessage <- "Please provide a dataframe or matrix for x!
 If in wide format, put genotypes as rows and environments
 as columns. If in long format, provide the names of the dataframe 
@@ -40,126 +45,127 @@ length 2 to the alpha argument with the first element as the significance
 threshold for the number of PCs,and the second as the significance threshold
 for the 'block' effect.\n"
 
-		isRCBD <- !is.null(block) | "block" %in% colnames(x)
-		anyNullGEy <- is.null(G) | is.null(E) | is.null(y)
-		allNullGEy <- is.null(G) & is.null(E) & is.null(y)
-		anyMissCells <- NULL
+	isRCBD <- !is.null(block) | "block" %in% colnames(x)
+	anyNullGEy <- is.null(G) | is.null(E) | is.null(y)
+	allNullGEy <- is.null(G) & is.null(E) & is.null(y)
+	anyMissCells <- NULL
 
-		if (is.null(x)){
-			if (anyNullGEy | !is.numeric(y)){
-				stop(warnmessage)
-			} else {
-				DF <- data.frame(G = G, E = E, y = y)
-				G <- "G"; E <- "E";	y <- "y"
-				if (isRCBD) {
-					DF$block <- block
-					block <- "block"
-				}
-			}
+	if (is.null(x)){
+		if (anyNullGEy | !is.numeric(y)){
+			stop(warnmessage)
 		} else {
-			isAllNum <- all(sapply(x,class) %in% c("numeric", "integer")) 
-
-			if (ncol(x) == 3 & anyNullGEy & isAllNum){
-				is3env <- "yes"
-				if(!override3col) is3env <- readline("You have provided a dataframe or matrix with 3 columns, and no variable names to 'G', 'E' and/or 'y'.
-							Is the provided dataframe or matrix contain 3 environments with genotypes on the rows?  (y/n) ")
-				if(override3col | is3env %in% c("y","yes","YES","Yes","Y")) {
-					x <- as.matrix(x)
-				} else {
-					stop(warnmessage)
-				}
+			DF <- data.frame(G = G, E = E, y = y)
+			G <- "G"; E <- "E";	y <- "y"
+			if (isRCBD) {
+				DF$block <- block
+				block <- "block"
 			}
+		}
+	} else {
+		isAllNum <- all(sapply(x,class) %in% c("numeric", "integer")) 
 
-			if (all(c("G","E","y") %in% colnames(x))){
-				DF <- as.data.frame(x[,c("G", "E", "y")])
-				G <- "G"; E <- "E";	y <- "y"
-				if(!class(DF[[y]]) %in% "numeric") DF[[y]] <- as.numeric(DF[[y]])
-				if(isRCBD) {
-					DF$block <- x[,"block"]
-					block <- "block"
-				}
-			} else  if (is.data.frame(x)){
+		if (ncol(x) == 3 & anyNullGEy & isAllNum){
+			is3env <- "yes"
+			if(!override3col) is3env <- readline("You have provided a dataframe or matrix with 3 columns, and no variable names to 'G', 'E' and/or 'y'.
+						Is the provided dataframe or matrix contain 3 environments with genotypes on the rows?  (y/n) ")
+			if(override3col | is3env %in% c("y","yes","YES","Yes","Y")) {
+				x <- as.matrix(x)
+			} else {
+				stop(warnmessage)
+			}
+		}
+
+		if (all(c("G","E","y") %in% colnames(x))){
+			DF <- as.data.frame(x[,c("G", "E", "y")])
+			G <- "G"; E <- "E";	y <- "y"
+			if(!class(DF[[y]]) %in% "numeric") DF[[y]] <- as.numeric(DF[[y]])
+			if(isRCBD) {
+				DF$block <- x[,"block"]
+				block <- "block"
+			}
+		} else  if (is.data.frame(x)){
+			DF <- x
+		} else if (is.matrix(x)){
+			if (is.numeric(x) & allNullGEy) {
+				if(any(is.na(x))) anyMissCells <- which(is.na(x), arr.ind = TRUE)
+				# DF <- melt(x)
+				DF <- meltName(x, "y")
+				# names(DF) <- c("G", "E", "y")
+				return(list(Y = x, DF = DF, isUnRep = TRUE, sigmasq = NULL, repPerG = 1, anyMissCells = anyMissCells))
+			} else if(!allNullGEy){
 				DF <- x
-			} else if (is.matrix(x)){
-				if (is.numeric(x) & allNullGEy) {
-					if(any(is.na(x))) anyMissCells <- which(is.na(x), arr.ind = TRUE)
-					DF <- melt(x)
-					names(DF) <- c("G", "E", "y")
-					return(list(Y = x, DF = DF, isUnRep = TRUE, sigmasq = NULL, repPerG = 1, anyMissCells = anyMissCells))
-				} else if(!allNullGEy){
-					DF <- x
-				}
-			} else {
-				stop(warnmessage)
-			}			
-		}
-		
-
-		Elvls <- unique(DF[[E]][!is.na(DF[[E]])])
-		Glvls <- unique(DF[[G]][!is.na(DF[[G]])])
-
-		DF[[E]] <- factor(DF[[E]], levels = Elvls)
-		DF[[G]] <- factor(DF[[G]], levels = Glvls)
-
-		repGE <- table(DF[[G]][!is.na(DF[[y]])], DF[[E]][!is.na(DF[[y]])])
-		nReps <- unique(repGE)
-		isUnRep <- length(nReps) == 1 & nReps[1] == 1
-		
-		I <- nrow(repGE)
-		J <- ncol(repGE)
-
-		if(I < 3 | J < 3) {stop(warnlackrank)}
-		
-		if (length(nReps) > 1){
-			cat(warnunbal)
-			if (any(repGE == 0)){
-				cat("This program now handles missing cells in the genotype environment table! An EM algorithm will be used to impute missing cells\n")
 			}
-		} 
-
-		if(isUnRep){
-			fit <- lm(as.formula(paste0(y," ~ ", E, " + ", G)), data = DF)
-			Y <- acast(DF, as.formula(paste0(G, " ~ ", E)), value.var = y)
 		} else {
-			if (isRCBD){	
-				fit <- lm(as.formula(paste0(y," ~ ", E, " + ", G, " + ", G, ":", E, " + ", block , ":", E)), data = DF)
-				pvalBlock <- anova(fit)["E:block","Pr(>F)"]
-				blockSig <- TRUE
-				if (length(alpha) > 1){
-					alphaBlock <- alpha[2] 
-					alpha <- alpha[1]
-				} else {
-					alphaBlock <- alpha
-				}
-				if (pvalBlock >= alphaBlock){
-					cat(blockNotSig)
-					blockSig <- FALSE
-					fit <- lm(as.formula(paste0(y," ~ ", E, " + ", G, " + ", G, ":", E)), data = DF)
-				}	
-			} else {
-				fit <- lm(as.formula(paste0(y," ~ ", E, " + ", G, " + ", G, ":", E)), data = DF)
-			}
-			coefs <- dummy.coef(fit)
-			intercept <- coefs[[1]]
-
-			Emat <- matrix(rep(coefs[[2]], I), nrow = I, ncol = J, byrow = TRUE)
-			Gmat <- matrix(rep(coefs[[3]], J), nrow = I, ncol = J)
-			GEmat <- matrix(coefs[[4]], nrow = I, ncol = J, byrow = TRUE)
-			
-			Y <- intercept + Gmat + Emat + GEmat
-			rownames(Y) <- Glvls
-			colnames(Y) <- Elvls
-		}
-		if(isUnRep) sigmasq <- NULL else sigmasq <- summary(fit)$sigma^2
-		errorDf <- anova(fit)["Residuals", "Df"]
-		returnList <- list(Y = Y, DF = DF, isUnRep = isUnRep)
-
-		if(!isUnRep){
-			sigmaList <- c(sigmasq = sigmasq, errorDf = errorDf)
-			returnList <- c(returnList, list(sigmasq = sigmaList))
-		}
-		if(length(nReps) > 1) repPerG <- repGE else repPerG <- nReps
-		if(isRCBD) returnList <- c(returnList, blockSig = blockSig)
-		returnList <- c(returnList, list(repPerG = repPerG), anyMissCells = anyMissCells)
-		return(returnList)
+			stop(warnmessage)
+		}			
 	}
+	
+
+	Elvls <- unique(DF[[E]][!is.na(DF[[E]])])
+	Glvls <- unique(DF[[G]][!is.na(DF[[G]])])
+
+	DF[[E]] <- factor(DF[[E]], levels = Elvls)
+	DF[[G]] <- factor(DF[[G]], levels = Glvls)
+
+	repGE <- table(DF[[G]][!is.na(DF[[y]])], DF[[E]][!is.na(DF[[y]])])
+	nReps <- unique(repGE)
+	isUnRep <- length(nReps) == 1 & nReps[1] == 1
+	
+	I <- nrow(repGE)
+	J <- ncol(repGE)
+
+	if(I < 3 | J < 3) {stop(warnlackrank)}
+	
+	if (length(nReps) > 1){
+		cat(warnunbal)
+		if (any(repGE == 0)){
+			cat("This program now handles missing cells in the genotype environment table! An EM algorithm will be used to impute missing cells\n")
+		}
+	} 
+
+	if(isUnRep){
+		fit <- lm(as.formula(paste0(y," ~ ", E, " + ", G)), data = DF)
+		Y <- acast(DF, as.formula(paste0(G, " ~ ", E)), value.var = y)
+	} else {
+		if (isRCBD){	
+			fit <- lm(as.formula(paste0(y," ~ ", E, " + ", G, " + ", G, ":", E, " + ", block , ":", E)), data = DF)
+			pvalBlock <- anova(fit)["E:block","Pr(>F)"]
+			blockSig <- TRUE
+			if (length(alpha) > 1){
+				alphaBlock <- alpha[2] 
+				alpha <- alpha[1]
+			} else {
+				alphaBlock <- alpha
+			}
+			if (pvalBlock >= alphaBlock){
+				cat(blockNotSig)
+				blockSig <- FALSE
+				fit <- lm(as.formula(paste0(y," ~ ", E, " + ", G, " + ", G, ":", E)), data = DF)
+			}	
+		} else {
+			fit <- lm(as.formula(paste0(y," ~ ", E, " + ", G, " + ", G, ":", E)), data = DF)
+		}
+		coefs <- dummy.coef(fit)
+		intercept <- coefs[[1]]
+
+		Emat <- matrix(rep(coefs[[2]], I), nrow = I, ncol = J, byrow = TRUE)
+		Gmat <- matrix(rep(coefs[[3]], J), nrow = I, ncol = J)
+		GEmat <- matrix(coefs[[4]], nrow = I, ncol = J, byrow = TRUE)
+		
+		Y <- intercept + Gmat + Emat + GEmat
+		rownames(Y) <- Glvls
+		colnames(Y) <- Elvls
+	}
+	if(isUnRep) sigmasq <- NULL else sigmasq <- summary(fit)$sigma^2
+	errorDf <- anova(fit)["Residuals", "Df"]
+	returnList <- list(Y = Y, DF = DF, isUnRep = isUnRep)
+
+	if(!isUnRep){
+		sigmaList <- c(sigmasq = sigmasq, errorDf = errorDf)
+		returnList <- c(returnList, list(sigmasq = sigmaList))
+	}
+	if(length(nReps) > 1) repPerG <- repGE else repPerG <- nReps
+	if(isRCBD) returnList <- c(returnList, blockSig = blockSig)
+	returnList <- c(returnList, list(repPerG = repPerG), anyMissCells = anyMissCells)
+	return(returnList)
+}
